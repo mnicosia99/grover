28c9ebcfde133b65fd47f783890626aa86c4f7c2
Mike Nicosia <mikenico@usc.edu>
2022-03-26 19:14:15 -0700

    added generation code

--- working/create_pdf.py
+++ working/create_pdf.py
@@ -6,5 +6,5 @@ def write_to_pdf(file_name, out_filepath):
     options = {
         "enable-local-file-access": ""
     }    
-    out_filename = file_name.replace(".heml", ".pdf")
-    pdfkit.from_file(file_name, out_filepath + os.sep + out_filename, options=options)
+    out_filename = file_name.replace(".html", ".pdf")
+    pdfkit.from_file(file_name, out_filepath + "/" + out_filename, options=options)
