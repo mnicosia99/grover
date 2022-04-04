3df5a96cc14b08e767e7fe16d04fb56fbc5e5f08
Mike Nicosia <mikenico@usc.edu>
2022-04-03 23:49:02 -0700

    added test-outputs

--- sample/encoder.py
+++ sample/encoder.py
@@ -124,7 +124,6 @@ class Encoder:
 
     def encode(self, text):
         bpe_tokens = []
-        print(text)
         for token in re.findall(self.pat, text):
             token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8'))
             bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(' '))
